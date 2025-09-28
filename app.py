36678fb0bdf62f416bf5a5f82efe120e88d686dd
Midhun-1-2 <kk8004419@gmail.com>
2025-09-29 00:57:01 +0530

    New1

--- app.py
+++ app.py
@@ -1,24 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for , flash
 from flask_mail import Mail, Message
 from datetime import datetime
-from dotenv import load_dotenv
-import os
 
-load_dotenv()
+
 app = Flask(__name__)
-app.secret_key = "midhun@123"
 
 
-app.config['MAIL_USERNAME'] = os.getenv('kk8004419@gmail.com')
-app.config['MAIL_PASSWORD'] = os.getenv('pcjv stvs tveu urcb')
 
-# Flask-Mail Configuration
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'kk8004419@gmail.com'      # Your email
-app.config['MAIL_PASSWORD'] = 'pcjv stvs tveu urcb'         # Use app password, not your real password
-app.config['MAIL_DEFAULT_SENDER'] = 'kk8004419@gmail.com'
+
 
 mail = Mail(app)
 @app.route("/")
