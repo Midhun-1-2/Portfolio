1ed28c968e3fa3cba413a8e36d264a43041e77a2
Midhun-1-2 <kk8004419@gmail.com>
2025-10-12 20:46:10 +0530

    update

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
-from flask_mail import Mail
-from datetime import datetime
+
+
 
 
 app = Flask(__name__)
@@ -9,10 +9,10 @@ app = Flask(__name__)
 
 
 
-mail = Mail(app)
+
 @app.route("/")
 def home():
-    current_year = datetime.now().year
+
     # Passing first 3 blog posts as preview on home page
     return render_template("index.html")
 
